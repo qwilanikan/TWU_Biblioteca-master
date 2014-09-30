c349e7e79ea42ab422dacc74f4b1b072d2da27b1
Alison Polton-Simon <alison.polton.simon@gmail.com>
2014-09-30 16:57:22 -0500

    Completed functional test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -11,6 +11,8 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         List<String> books = new ArrayList<String>();
         books.add("Clean Code");
+        books.add("Agile Samurai");
+        books.add("Joy of Cooking");
 
         Library library = new Library(System.out, books);
         library.listBooks();
